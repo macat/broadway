15e767dde8406ce100b8728858d6238474779e2a
Jim Walker <jim.walker@namely.com>
2016-04-08 17:39:18 -0400

    start implementing type check for pods

--- deployment/pod_manifest_step.go
+++ deployment/pod_manifest_step.go
@@ -6,6 +6,7 @@ import (
 
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/v1"
+	"k8s.io/kubernetes/pkg/conversion"
 	"k8s.io/kubernetes/pkg/fields"
 	"k8s.io/kubernetes/pkg/runtime"
 )
@@ -33,6 +34,19 @@ func (s *PodManifestStep) Deploy() error {
 	}
 
 	o := s.object.(*v1.Pod)
+	p, err := client.Pods(namespace).Get(o.ObjectMeta.Name)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println(p)
+	if p.ObjectMeta.Name == o.ObjectMeta.Name {
+		log.Println("Deleting old pod: ", o.ObjectMeta.Name)
+		err = client.Pods(namespace).Delete(o.ObjectMeta.Name, nil)
+		if err != nil {
+			return err
+		}
+	}
+
 	log.Println("Creating new pod: ", o.ObjectMeta.Name)
 	_, err = client.Pods(namespace).Create(o)
 	if err != nil {
@@ -46,9 +60,20 @@ func (s *PodManifestStep) Deploy() error {
 	watcher, err := client.Pods(namespace).Watch(lo)
 	defer watcher.Stop()
 	var pod *v1.Pod
+
 	for {
 		event := <-watcher.ResultChan()
+		log.Printf("%T", event)
+		err = DefaultConvert(event.Object.(*api.Pod), pod, flags, meta)
+		if err != nil {
+			log.Println("conversion ", err)
+		}
+		// switch event.(type) {
+		// case *v1.:
 		pod = event.Object.(*v1.Pod)
+		// case *api.Pod:
+		// 	v1.Convert_api_Pod_To_v1_Pod(event.Object.(*api.Pod), pod)
+		// }
 
 		if pod.Status.Phase != v1.PodPending && pod.Status.Phase != v1.PodRunning {
 			log.Println("NOT PENDING AND NOT RUNNING")
@@ -64,6 +89,10 @@ func (s *PodManifestStep) Deploy() error {
 	if pod.Status.Phase == v1.PodUnknown {
 		log.Println("Setup pod failed: ", o.ObjectMeta.Name)
 		return errors.New("State of Pod Unknown")
+	}
+	err = client.Pods(namespace).Delete(o.ObjectMeta.Name, nil)
+	if err != nil {
+		return err
 	}
 
 	return nil
--- services/deployment_test.go
+++ services/deployment_test.go
@@ -23,15 +23,37 @@ func TestDeployment(t *testing.T) {
 
 	service := NewDeploymentService(store.New(), playbooks, manifests)
 
-	i := &instance.Instance{
-		PlaybookID: "hello",
-		ID:         "test",
-		Vars: map[string]string{
-			"version": "test",
+	cases := []struct {
+		Name     string
+		Instance *instance.Instance
+		Error    error
+		Expected instance.Status
+	}{
+		{
+			Name: "Good Playbook",
+			Instance: &instance.Instance{
+				PlaybookID: "hello",
+				ID:         "test",
+				Vars: map[string]string{
+					"version": "test",
+				},
+			},
+			Error:    nil,
+			Expected: instance.StatusDeployed,
 		},
 	}
 
-	err = service.Deploy(i)
-	assert.Nil(t, err)
-	assert.EqualValues(t, instance.StatusDeployed, i.Status)
+	// i := &instance.Instance{
+	// 	PlaybookID: "hello",
+	// 	ID:         "test",
+	// 	Vars: map[string]string{
+	// 		"version": "test",
+	// 	},
+	// }
+
+	for _, c := range cases {
+		err = service.Deploy(c.Instance)
+		assert.Equal(t, c.Error, err)
+		assert.EqualValues(t, c.Expected, c.Instance.Status)
+	}
 }
