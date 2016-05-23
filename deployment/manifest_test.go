963705155b538ee406ff674b76e7494df204ccc7
Attila Maczak <attila@maczak.hu>
2016-05-23 16:49:20 -0400

    adding template functions to manifest templates

--- deployment/manifest_test.go
+++ deployment/manifest_test.go
@@ -7,10 +7,30 @@ import (
 )
 
 func TestExecute(t *testing.T) {
-	m, err := NewManifest("test", `{{ .test }}`)
-	assert.Nil(t, err)
+	cases := []struct {
+		name     string
+		template string
+		vars     map[string]string
+		expected string
+	}{
+		{
+			name:     "simple",
+			template: `{{ .test }}`,
+			vars:     map[string]string{"test": "hello!"},
+			expected: "hello!",
+		},
+		{
+			name:     "template",
+			template: `{{ $cities := split .test ","}}{{ join $cities "+" }}`,
+			vars:     map[string]string{"test": "brooklyn,manhattan"},
+			expected: "brooklyn+manhattan",
+		},
+	}
 
-	out := m.Execute(map[string]string{"test": "hello!"})
-
-	assert.Equal(t, "hello!", out)
+	for _, c := range cases {
+		m, err := NewManifest(c.name, c.template)
+		assert.Nil(t, err)
+		out := m.Execute(c.vars)
+		assert.Equal(t, c.expected, out, c.name+" case does not match output")
+	}
 }
